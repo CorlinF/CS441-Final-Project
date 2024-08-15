65a66d3964ab42c6a9f673f32221be22f0a53986
zhihw <wzhlephone@gmail.com>
2024-08-15 16:29:56 -0700

    node and tree

--- MCTS.py
+++ MCTS.py
@@ -1,1 +1,36 @@
+from board import Board
+
+class McNode:
+    def __init__(self,board,parent=None):
+        self.board=board
+        self.parent=parent
+        self.children=[]
+        self.visited=0
+        self.value=0
+
+    def is_fully_expanded():
+        pass
+
+    def select():
+        pass
+
+    def rollout():
+        pass
+
+    def backpropagate():
+        pass
+
+    def expand():
+        pass
+
+
+class MCTS:
+    def __init__(self,board,time):
+        self.root=board
+        self.time=time#Set time limit
+
+    def search():
+        pass
+
+    
 
