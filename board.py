edf67ce368b0930278416b9bd097936a4d3f2188
CorlinF <166881173+CorlinF@users.noreply.github.com>
2024-08-15 19:20:38 -0700

    Update board.py

--- board.py
+++ board.py
@@ -94,6 +94,6 @@ class Board:
             x1,x2 = choice(range(8)),choice(range(8))
             if self.state[x1][x2] in [team,10*team]:
                 piece = self.Piece((i,j),self.state[x1][x2])
-                y1,y2 = choice([x1-team,x1-2*team]),choice([x2+1,x2-1,x2+2,x2-2])
+                y1,y2 = choice([x1+1,x1-1,x1+2,x1-2]),choice([x2+1,x2-1,x2+2,x2-2])
                 if is_legal(piece,(y1,y2)):
                     return (piece,(y1,y2))
