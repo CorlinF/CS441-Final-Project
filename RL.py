4a119c1c27e1eb887746d420460349854be51b9f
CorlinF <166881173+CorlinF@users.noreply.github.com>
2024-08-16 10:39:13 -0700

    Create RL.py

---
+++ RL.py
@@ -0,0 +1,13 @@
+class RL:
+  def __init__(self,board):
+    self.board = board
+    self.Q = {}
+
+  def select(self,epsilon):
+    pass
+
+  def best_move(self):
+    pass
+
+  def train(self,N,M,eta,gamma):
+    pass
