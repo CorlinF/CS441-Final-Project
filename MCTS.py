f680c024d8ad55b8a7de5edf060e45deec9249cb
zhihw <wzhlephone@gmail.com>
2024-08-15 23:59:09 -0700

    search

--- MCTS.py
+++ MCTS.py
@@ -1,6 +1,7 @@
 from board import Board
 from random import choice
 from math import sqrt, log
+import time
 
 class McNode:
     def __init__(self,board,team,parent=None):
@@ -28,6 +29,7 @@ class McNode:
             self.value += 1
         elif result == 0:
             self.value += .5
+        return result
 
     def backpropagate(self,winner):
         self.visited += 1
@@ -43,12 +45,30 @@ class McNode:
 
 
 class MCTS:
-    def __init__(self,board,time):
-        self.root=board
+    def __init__(self,board,team,time):
+        self.root=McNode(board,team)
         self.time=time#Set time limit
 
-    def search():
-        pass
+    def search(self):
+        start_time = time.time()
+        while time.time() - start_time < self.time_limit:
+            current=self.root
+            while current.children:
+                current=current.select()
+            if current.visited==0:
+                    result=current.rollout
+            else:
+                current.expand()
+                current=current.select()
+                result=current.rollout()
+            current.backpropagate(result)
+        best_child = max(self.root.children, key=lambda c: c.visited)
+        return best_child.board
+    
+
+
+
+        
 
     
 
